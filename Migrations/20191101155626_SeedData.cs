be573f659591d2523c0d53f98813de76cff1c3bc
Mike <mmcshane10@gmail.com>
2019-11-01 08:57:47 -0700

    add first entry of seed data

---
+++ Migrations/20191101155626_SeedData.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NationalParks.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Parks",
+                columns: new[] { "ParkId", "Description", "Established", "ImageURL", "Name", "Size", "States", "Visitors" },
+                values: new object[] { 1, "The Grand Canyon, carved by the mighty Colorado River, is 277 miles (446 km) long, up to 1 mile (1.6 km) deep, and up to 15 miles (24 km) wide. Millions of years of erosion have exposed the multicolored layers of the Colorado Plateau in mesas and canyon walls, visible from both the north and south rims.", "February 26, 1919 ", "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/USA_09847_Grand_Canyon_Luca_Galuzzi_2007.jpg/200px-USA_09847_Grand_Canyon_Luca_Galuzzi_2007.jpg", "Grand Canyon", "1,201,647.03 acres", "Arizona", "6,380,495 " });
+
+            migrationBuilder.InsertData(
+                table: "Parks",
+                columns: new[] { "ParkId", "Description", "Established", "ImageURL", "Name", "Size", "States", "Visitors" },
+                values: new object[] { 2, "", "", "", "", "", "", "" });
+
+            migrationBuilder.InsertData(
+                table: "Parks",
+                columns: new[] { "ParkId", "Description", "Established", "ImageURL", "Name", "Size", "States", "Visitors" },
+                values: new object[] { 3, "", "", "", "", "", "", "" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 3);
+        }
+    }
+}
--- Models/NationalParksContext.cs
+++ Models/NationalParksContext.cs
@@ -10,5 +10,17 @@ namespace NationalParks.Models
         }
 
         public DbSet<Park> Parks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Park>()
+                .HasData(
+                    new Park { ParkId = 1, Name = "Grand Canyon", Description = "The Grand Canyon, carved by the mighty Colorado River, is 277 miles (446 km) long, up to 1 mile (1.6 km) deep, and up to 15 miles (24 km) wide. Millions of years of erosion have exposed the multicolored layers of the Colorado Plateau in mesas and canyon walls, visible from both the north and south rims.", Established = "February 26, 1919 ", States = "Arizona", Size = "1,201,647.03 acres", Visitors = "6,380,495 ", ImageURL = "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/USA_09847_Grand_Canyon_Luca_Galuzzi_2007.jpg/200px-USA_09847_Grand_Canyon_Luca_Galuzzi_2007.jpg" },
+
+                    new Park { ParkId = 2, Name = "", Description = "", Established = "", States = "", Size = "", Visitors = "", ImageURL = "" },
+
+                    new Park { ParkId = 3, Name = "", Description = "", Established = "", States = "", Size = "", Visitors = "", ImageURL = "" }
+                );
+        }
     }
 }
