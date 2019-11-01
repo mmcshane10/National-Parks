6254fcb02eaee6599aba29906c5b0c5b375249a6
Mike <mmcshane10@gmail.com>
2019-11-01 08:47:50 -0700

    add PUT and DELETE methodsto controller.

--- Controllers/ParksController.cs
+++ Controllers/ParksController.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using NationalParks.Models;
 
 namespace NationalParks.Controllers
@@ -23,11 +24,36 @@ namespace NationalParks.Controllers
             return _db.Parks.ToList();
         }
 
+        // GET api/parks/5
+        [HttpGet("{id}")]
+        public ActionResult<Park> Get(int id)
+        {
+            return _db.Parks.FirstOrDefault(entry => entry.ParkId == id);
+        }
+
         // POST api/parks
         [HttpPost]
         public void Post([FromBody] Park park)
         {
             _db.Parks.Add(park);
+            _db.SaveChanges();
+        }
+
+        // PUT api/parks/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Park park)
+        {
+            park.ParkId = id;
+            _db.Entry(park).State = EntityState.Modified;
+            _db.SaveChanges();
+        }
+
+        // DELETE api/parks/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var parkToDelete = _db.Parks.FirstOrDefault(entry => entry.ParkId == id);
+            _db.Parks.Remove(parkToDelete);
             _db.SaveChanges();
         }
     }
